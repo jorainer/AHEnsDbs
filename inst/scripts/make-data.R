839626d73a8d2d26c9627821918f29d061a6d453
jotsetung <johannes.rainer@gmail.com>
2017-01-13 20:16:41 +0100

    Add description of the process to create the EnsDbs

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -2,16 +2,32 @@
 ## Creation of the EnsDb sqlite files will be performed manually by me with
 ## each new release.
 
-library(rtracklayer)
+## Requirements:
+## o Perl
+## o Ensembl Perl API corresponding to the version for which the databases should
+##   be created. See http://www.ensembl.org/info/docs/api/api_installation.html
+##   for more details.
+## o BioPerl.
+## o MySQL server with access credentials allowing to create databases and insert
+##   data.
+##
+## Description:
+## Needs the Ensembl Perl API and BioPerl to be installed as well as a local
+## MySQL server on which temporarily the Ensembl core database will be installed.
+## Generation of EnsDb SQLite databases are generated using the
+## `createEnsDbForSpecies` function in the generate-EnsDbs.R script available in
+## the inst/scripts folder of the ensembldb package.
+## This function first connects to the Ensembl ftp server and fetches the names
+## of all species for which an Ensembl core database is available. Subsequently
+## it processes all species by
+## 1) Downloading the MySQL core database dump for the species.
+## 2) Locally install this MySQL database
+## 3) Using the corresponding functions of the ensembldb package extract all
+##    relevant data from this database and generate the EnsDb SQLite file. The
+##    R functions use a perl script from the ensembldb package to fetch the data
+##    using the Ensembl Perl API.
+##
+## Notes: it would be possible to create all databases by directly querying the
+## core database directly from Ensembl, but installing local MySQL instances has
+## been proven to be faster and a more reliable solution.
 
-parseCytoBands <- function(url) {
-    con <- gzcon(url(url), text=TRUE)
-    tab <- read.table(con, sep="\t",
-                      col.names=c("seqnames", "start", "end", "name",
-                                  "gieStain"))
-    makeGRangesFromDataFrame(tab, keep.extra.columns = TRUE,
-                             starts.in.df.are.0based = TRUE)
-}
-meta <- AnnotationHubData::readMetadataFromCsv("../..")
-cytoBands <- lapply(meta$SourceUrl, parseCytoBands)
-mapply(saveRDS, cytoBands, file=meta$ResourceName)
