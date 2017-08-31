11b9ef26b897f5d33ab5e00ba8999c4be6b4065b
jotsetung <johannes.rainer@gmail.com>
2017-08-31 07:00:29 +0200

    Add Ensembl 90 EnsDb databases

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -5,11 +5,11 @@
 ## 3) Amend the `ensemblVersion` and `baseDir` variables to point to that folder.
 
 ## ensemblVersion: the Ensembl version
-ensemblVersion <- 89
+ensemblVersion <- 90
 
 ## baseDir amend the base path to local directory. The default settings point
 ## to a base folder "EnsDbs" located in the same directory than the AHEnsDbs.
-baseDir <- paste0("../../../EnsDbs/", ensemblVersion, "/")
+baseDir <- paste0("../../../../EnsDbs/", ensemblVersion, "/")
 
 
 ## Start processing the data.
