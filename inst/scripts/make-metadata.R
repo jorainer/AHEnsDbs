1b5c7d2ba8bbb5819f68b66f28675338ae520f57
jotsetung <johannes.rainer@gmail.com>
2019-01-14 08:12:10 +0100

    Update to Ensembl 95

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -5,7 +5,7 @@
 ## 3) Amend the `ensemblVersion` and `baseDir` variables to point to that folder.
 
 ## ensemblVersion: the Ensembl version
-ensemblVersion <- 94
+ensemblVersion <- 95
 biocVersion <- "3.8"
 
 ## baseDir amend the base path to local directory. The default settings point
