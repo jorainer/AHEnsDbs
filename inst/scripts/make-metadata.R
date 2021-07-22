52d545f0e044e8addee9abfdbba212cd6ec358d6
jorainer <johannes.rainer@gmail.com>
2021-07-22 08:27:53 +0200

    Add Ensembl release 104 metadata

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -5,8 +5,8 @@
 ## 3) Amend the `ensemblVersion` and `baseDir` variables to point to that folder.
 
 ## ensemblVersion: the Ensembl version
-ensemblVersion <- 102
-biocVersion <- "3.12"
+ensemblVersion <- 104
+biocVersion <- "3.13"
 
 ## baseDir amend the base path to local directory. The default settings point
 ## to a base folder "EnsDbs" located in the same directory than the AHEnsDbs.
