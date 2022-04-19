f5d301347ea2b6bce21b4278a4b5fd414a33599c
jorainer <johannes.rainer@gmail.com>
2022-04-19 13:23:04 +0200

    Add Ensembl 106 databases

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -5,7 +5,7 @@
 ## 3) Amend the `ensemblVersion` and `baseDir` variables to point to that folder.
 
 ## ensemblVersion: the Ensembl version
-ensemblVersion <- 105
+ensemblVersion <- 106
 biocVersion <- "3.14"
 
 ## baseDir amend the base path to local directory. The default settings point
